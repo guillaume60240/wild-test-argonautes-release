c85b2af947c033b82f3352de8958a2c45035ab85
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-12-10 13:10:54 +0100

    add 404 redirection

---
+++ public/error/404.php
@@ -0,0 +1,24 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5; URL=https://www.sitetestn.fr/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+    <title>Le voyage de Jason - erreur 404</title>
+</head>
+<body class=" vh-100">
+
+    <?php include '../../src/template/partials/header.php'; ?>
+
+    <div class="text-center m-5">
+        <h1 class="mt-5 mb-5">Alors Jason!! Qu'est ce que tu fais là?</h1>
+        <p>Tu vas être redirigé dans quelques secondes !</p>
+        <a href="/" class="link-success">Sinon retourne à la maison en cliquant ici !</a>
+    </div>
+
+    
+    <?php include '../../src/template/partials/footer.php'; ?>
+    
+
+</body>
--- src/template/partials/list.php
+++ src/template/partials/list.php
@@ -1,3 +1,3 @@
-<div class="col-6">
+<div class="col-6 col-md-4">
   <h5 class="text-start"><?= $argonaute['id'] ?> - <?= ucwords($argonaute['firstName'])?></h5>
 </div>
